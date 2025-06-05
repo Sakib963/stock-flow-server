7de828277dc5ce763d7ddfc1e37a578ab14b3c17
Sakib963 <abdullah15-963@diu.edu.bd>
2025-06-06 03:38:33 +0600

    hotfix: log issue

--- src/utils/log.js
+++ src/utils/log.js
@@ -2,7 +2,7 @@ require("winston-daily-rotate-file");
 const path = require("path");
 const winston = require("winston");
 
-const isFileLoggingEnabled = true;
+const isFileLoggingEnabled = false;
 const PROJECT_ROOT = path.join(__dirname, "..");
 const transports = [];
 
